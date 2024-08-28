4791b2506312578e9d80d95572813dcea8c714a3
PizaaRiaaa <mm.rikkimae@gmail.com>
2024-08-28 22:17:43 +0800

    Added delete all and localstorage retrieval

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-let myLeads = [];
+let myLeads = ["www.facebook.com"];
 
 let saveInputBtn = document.querySelector(".input-btn");
+let btnDelete = document.querySelector(".delete-btn");
 let inputEl = document.querySelector(".input-el")
 let boxDiv = document.querySelector(".box");
 let ulEl = document.querySelector(".ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
 saveInputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
   inputEl.value = "";
-
   renderLeads();
+})
 
+btnDelete.addEventListener("click", function() {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads()
 })
 
 function renderLeads() {
   let listItems = "";
   for(let i = 0; i < myLeads.length; i++) {
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
     listItems += `
       <li><a target='_blank' href=${myLeads[i]}>  ${myLeads[i]} </a></li>`; 
   }
